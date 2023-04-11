f43b5f1b604a5fb451bf10d9ec4469e3cf9d27fa
shivamgauswami <130463011+shivamgauswami@users.noreply.github.com>
2023-04-11 17:12:42 +0530

    Add files via upload

---
+++ shivam2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+main()
+{
+	printf("- - - - - \n");
+	printf("|\t|\n");
+	printf("s\t|\n");
+	printf("h\t|\n");
+	printf("i\t|\n");
+	printf("v\t|\n");
+	printf("a\t|\n");
+	printf("m\t|\n");
+    printf("|\t|\n");		
+	printf("- - - - - \n");
+	
+}
---
+++ shivam3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+main()
+{
+	printf("*\n");
+	printf("*\t*\n");
+	printf("*\t*\t*\n");
+	printf("*\t*\n");
+	printf("*\n");
+	
+}
